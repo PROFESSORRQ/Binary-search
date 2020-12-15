9cd1b47d7a19843fcfff06970e59233d3eba757e
ANSH GUPTA <anshgupta661@gmail.com>
2020-12-15 17:50:50 +0530

    Binary Search Algoritm

---
+++ Binary Search.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<hashtable.h>
+#include<math.h>
+#include<vector>
+#include<string.h>
+using namespace std;
+#define Nmax INT_MAX;
+#define Nmin INT_MIN;
+// void print(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+       
+//     }
+// }
+int binarySearch(int arr[],int n, int key){
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(key>arr[mid]){
+            s=mid+1;
+        }
+        else if(key<arr[mid])
+        e=mid-1;
+        else
+        {
+            return mid;
+        }
+        
+    }
+    return -1;
+}
+int main()
+{
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    int n, i, j, k, p, l, count = 0, flag = 0;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin>>p;
+    cout<<binarySearch(arr,n,p);
+
+    return 0;
+}
